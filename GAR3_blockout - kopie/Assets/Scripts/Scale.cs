a86c8f4976b7ad1c0e8e4e1a5ed5caf381beacd6
Ilja6104 <ilja.ham@gmail.com>
2020-04-09 09:30:27 +0200

    lot of changes

---
+++ GAR3_blockout - kopie/Assets/Scripts/Scale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Scale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.localScale = transform.localScale/100;
+        transform.eulerAngles = new Vector3(90,0,0);
+        //transform.localRotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
